4773742b08e22976b53989d174913a6c59838476
Neeraj Baid <wdaareg@gmail.com>
2014-11-14 18:14:22 -0800

    Fix implicit declaration for calcDepth.

--- calcDepthOptimized.c
+++ calcDepthOptimized.c
@@ -20,18 +20,6 @@ float displacement(int dx, int dy) {
     return displacement;
 }
 
-void calcDepthOptimized(float *depth,
-                        float *left,
-                        float *right,
-                        int imageWidth,
-                        int imageHeight,
-                        int featureWidth,
-                        int featureHeight,
-                        int maximumDisplacement) {
-    calcDepth(depth, left, right, imageWidth, imageHeight,
-              featureWidth, featureHeight, maximumDisplacement, NULL);
-}
-
 void calcDepth(float *depth,
                float *left,
                float *right,
@@ -107,4 +95,16 @@ void calcDepth(float *depth,
             }
         }
     }
+}
+
+void calcDepthOptimized(float *depth,
+                        float *left,
+                        float *right,
+                        int imageWidth,
+                        int imageHeight,
+                        int featureWidth,
+                        int featureHeight,
+                        int maximumDisplacement) {
+    calcDepth(depth, left, right, imageWidth, imageHeight,
+              featureWidth, featureHeight, maximumDisplacement, NULL);
 }
